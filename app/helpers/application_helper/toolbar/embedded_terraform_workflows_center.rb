291ee472718a20a08b249454003cc76cd913dff8
Adam Grare <adam@grare.com>
2024-02-19 14:47:22 -0500

    Add EmbeddedTerraform UI

---
+++ app/helpers/application_helper/toolbar/embedded_terraform_workflows_center.rb
@@ -0,0 +1,43 @@
+class ApplicationHelper::Toolbar::EmbeddedTerraformWorkflowsCenter < ApplicationHelper::Toolbar::Basic
+  button_group('workflows_policy', [
+                 select(
+                   :embedded_terraform_workflows_configuration,
+                   nil,
+                   t = N_('Configuration'),
+                   t,
+                   :items => [
+                     button(
+                       :embedded_configuration_script_payload_map_credentials,
+                       'pficon pficon-edit fa-lg',
+                       t = N_('Map Credentials to this Workflow'),
+                       t,
+                       :klass        => ApplicationHelper::Button::EmbeddedTerraform,
+                       :enabled      => false,
+                       :onwhen       => "1",
+                       :url_parms    => "edit_div",
+                       :send_checked => true
+                     ),
+                   ]
+                 ),
+                 select(
+                   :embedded_terraform_workflows_policy_choice,
+                   nil,
+                   t = N_('Policy'),
+                   t,
+                   :enabled => false,
+                   :onwhen  => "1+",
+                   :items   => [
+                     button(
+                       :embedded_configuration_script_payload_tag,
+                       'pficon pficon-edit fa-lg',
+                       N_('Edit Tags for the selected Workflows'),
+                       N_('Edit Tags'),
+                       :url_parms    => "main_div",
+                       :send_checked => true,
+                       :enabled      => false,
+                       :onwhen       => "1+"
+                     ),
+                   ]
+                 )
+               ])
+end
---
+++ app/helpers/embedded_terraform_workflow_helper.rb
@@ -0,0 +1,3 @@
+module EmbeddedTerraformWorkflowHelper
+  include TextualSummary
+end
